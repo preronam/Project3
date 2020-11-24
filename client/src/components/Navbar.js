3e0c43ed781bd73ee53f76db37a21089402c90df
Michael Jones <michaeljones@Michaels-MacBook-Pro.local>
2020-11-24 16:28:31 -0600

    passport authentication mongodb setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Redirect } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import axios from 'axios'
+
 import {
   AppBar,
   Toolbar,
--- server.js
+++ server.js
@@ -1,24 +1,79 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const dbConnection = require('./database'); 
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Route Requires//
+const user = require('./routes/user')
+
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+
+// Sessions//
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+// Passport 
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+
+// Routes
+app.use('/user', user)
+
 // Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+
+// Starting Server 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+	console.log(`App listening on PORT: ${PORT}`)
 });
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
